bc0feeae999e553a0a4b1557d285c78472de08e2
Apaar <apaarshrm39@gmail.com>
2022-01-10 21:19:51 +0530

    middleware files

--- html/cmd/web/routes.go
+++ html/cmd/web/routes.go
@@ -5,14 +5,32 @@ import (
 
 	"example.com/html/pkg/config"
 	"example.com/html/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
+// Pat Router
 func Route(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
+	/*
+		mux := pat.New()
+		mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+		mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+		return mux
+	*/
+
+	// Chi Router
+
+	mux := chi.NewRouter()
+
+	// middleware!
+	mux.Use(middleware.Recoverer)
+	//custom middleware
+	mux.Use(NoSurf)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 
 }
